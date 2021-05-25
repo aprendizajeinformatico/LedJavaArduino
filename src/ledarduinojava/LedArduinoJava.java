2eb305da5d47e0be5186bfff4c8837922f22281e
A-Informático <A-Informtico@David>
2021-05-24 20:07:01 -0400

    Subiendo proyecto a GitHub

---
+++ src/ledarduinojava/LedArduinoJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ledarduinojava;
+
+import Views.Led;
+
+/**
+ *
+ * @author A-Informático
+ */
+public class LedArduinoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Led view = new Led();
+        view.setVisible(true);
+    }
+    
+}
